4d6acfbc6401479a85ea8b160157849d597758bf
TaweelapTop <117077117+TaweelapTop@users.noreply.github.com>
2022-12-14 10:20:45 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,15 +14,18 @@ class UserController extends Controller
         //   where email_verified_at is not null
         //   order by created_at desc
         //   limit 3
-
-        $users = User::all(); // replace this with Eloquent statement
+       
+        // $users = User::all(); // replace this with Eloquent statement
+         $users = User::where('email_verified', not null)
+            ->orderByDesc('created_at')
+            ->limit(3);
 
         return view('users.index', compact('users'));
     }
 
     public function show($userId)
     {
-        $user = NULL; // TASK: find user by $userId or show "404 not found" page
+        $user = User::findOfFail($userId); // TASK: find user by $userId or show "404 not found" page
 
         return view('users.show', compact('user'));
     }
@@ -40,7 +43,11 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and update it with $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
+        $user = User::firstOrCreate(
+            ['name' => '$name'],
+            ['email' => '$email'],
+            ['password' => 'random()']
+        ); // updated or created user
 
         return view('users.show', compact('user'));
     }
